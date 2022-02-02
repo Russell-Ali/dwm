8d0acd0df7f264aa003e422188c84eeffd590d57
Russell-Ali <resul.eliyev2000@mail.ru>
2022-02-02 09:14:12 +0400

    First commit

---
+++ README.d
@@ -0,0 +1,16 @@
+List of patches applied:
+dwm-actualfullscreen-20211013-cb3f58a.diff
+dwm-alwayscenter-20200625-f04cac6.diff
+dwm-attachbottom-6.2.diff
+dwm-autostart-20161205-bb3bd6f.diff
+dwm-bar-height-6.2.diff
+dwm-focusonclick-20200110-61bb8b2.diff
+dwm-monoclesymbol-6.2.diff
+dwm-movestack-20211115-a786211.diff
+dwm-pertag-20200914-61bb8b2.diff
+dwm-restartsig-20180523-6.2.diff
+dwm-scratchpad-20200727-bb2e7222baeec7776930354d0e9f210cc2aaad5f.diff
+dwm-swallow-20201211-61bb8b2.diff
+dwm-tapresize-20200819-f04cac6.diff
+(Also in patches directory)
+Plus they are little changed here and there mostly.
