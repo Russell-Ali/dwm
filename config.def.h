5208c600a335b9f89fcdf0ed660d2502996fac3e
russell-ali <resul.eliyev2000@mail.ru>
2022-02-21 01:37:48 +0400

    @

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-bw", "2", "-l", "16", "-h", "28", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "90x24", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
