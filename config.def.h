6b0e68a969652bf0034d0fcace5b98fbb36381b6
Russell-Ali <resul.eliyev2000@mail.ru>
2022-03-01 13:36:59 +0400

    defaults for dmenu

--- config.def.h
+++ config.def.h
@@ -9,7 +9,6 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Dejavu-12" };
-static const char dmenufont[]       = "Dejavu-12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,6 +31,7 @@ static const Rule rules[] = {
 	/* class    instance    title       tags mask   iscentered  isfloating      isterminal      noswallow       monitor */
 	{ "Pcmanfm","pcmanfm",NULL,              0,         1,        1,              0,              1,              -1 },
     { "Galculator","galculator",NULL,        0,         1,        1,              0,              1,              -1 },
+    { "Timeshift-gtk","timeshift-gtk",NULL,  0,         1,        1,              0,              1,              -1 },
 	{ "st-256color","st-256color",NULL,      0,         0,        0,              1,              0,              -1 },
 	{  NULL,NULL,"Event Tester",             0,         0,        0,              0,              1,              -1 }, /* xev */
 };
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-bw", "2", "-l", "16", "-h", "28", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "90x24", NULL };
